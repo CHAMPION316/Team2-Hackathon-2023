5692761dc4809a04af71724a03c14658a784eaaa
sean <seanyoung247@gmail.com>
2023-08-19 12:02:30 +0100

    feat: Adds test level

---
+++ src/test-level.js
@@ -0,0 +1,76 @@
+
+import game, { SCALE } from './game';
+import * as warped from './warped';
+
+// platforms
+addLevel([
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    '                                                                      ',
+    ' <==================================================================> '
+], {
+    tileWidth: 16,
+	tileHeight: 16,
+    tiles: {
+        '<': () => [
+            sprite("platform-l"),
+            area(),
+            scale(SCALE),
+            body({ isStatic: true }),
+            tile({ isObstacle: true }),
+        ],
+        '=': () => [
+            sprite("platform-m"),
+            area(),
+            scale(SCALE),
+            body({ isStatic: true }),
+            tile({ isObstacle: true }),
+        ],
+        '>': () => [
+            sprite("platform-r"),
+            area(),
+            scale(SCALE),
+            body({ isStatic: true }),
+            tile({ isObstacle: true }),
+        ],
+    }
+});
