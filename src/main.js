4c46f4752590d5ea1b15dff05c7cb17b671a9f13
sean <seanyoung247@gmail.com>
2023-08-20 22:46:49 +0100

    feat: Adds camera scaling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-import { SCALE } from './game';
+import { SCALE, CAMERA_SCALE } from './game';
 import { getPlayer } from './player'
 import * as level1 from './levels/level-1'
 import setupGreenGuy from './enemy';
@@ -9,6 +9,7 @@ import setupGreenGuy from './enemy';
 scene('game', () => {
 	setGravity(640 * SCALE);
 
+	camScale(CAMERA_SCALE, CAMERA_SCALE);
 	const level = level1.loadLevel();
 	const player = getPlayer(level);
 	setupGreenGuy(level);
